c74bdc787ed1cec03be85d394790aa3207eb91c3
Arnaud293 <arnaudlt293@gmail.com>
2022-09-27 15:57:12 +0200

    Create error display function

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const inputs = document.querySelectorAll('input[type="text"], input[type="password"]');
 
+const errorDisplay = (tag, message, valid) => {
+    const container = document.querySelector("." + tag + '-container');
+    const span = document.querySelector("." + tag + '-container > span');
+
+    if(!valid){
+        container.classList.add('error');
+        span.textContent = message;
+    } else {
+        container.classList.remove('error');
+        span.textContent = '';
+    }
+}
 
 const pseudoChecker= (value) => {
-   
+   if(value.length > 0 && (value.length< 3 || value.length >20)){
+        errorDisplay("pseudo", "Le pseudo doit contenir entre 3 et 20 caractères")
+   }
 }
 
 const emailChecker= (value) => {
